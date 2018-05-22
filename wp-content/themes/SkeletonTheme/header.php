567d3865c5bc531708cb2165baefdb3b114da6ca
Paul Wr <paul.w35@live.com>
2018-05-22 11:53:45 +0000

    files updated

--- wp-content/themes/SkeletonTheme/header.php
+++ wp-content/themes/SkeletonTheme/header.php
@@ -5,5 +5,6 @@
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <?php wp_head(); ?> <!-- embed wordpress header -->
 </head>
-<body>
+<body <?php body_class();?> > <!-- add wp body classes -->
     <h1>This is the header</h1>
+    <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'your-nav-class')) ?> <!-- call menu and specify where to find -->
--- wp-content/themes/SkeletonTheme/index.php
+++ wp-content/themes/SkeletonTheme/index.php
@@ -1,3 +1,14 @@
 <?php get_header(); ?>
-    <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'your-nav-class')) ?> <!-- call menu and specify where to find -->
+    
+    <?php 
+    
+    if( have_posts() ):
+        while( have_posts() ): the_post(); ?>
+            <h1><?php the_title(); ?></h1>
+            <p><?php the_content(); ?></p>
+        <?php endwhile;
+    endif;
+    
+    ?>
+    
 <?php get_footer(); ?>
