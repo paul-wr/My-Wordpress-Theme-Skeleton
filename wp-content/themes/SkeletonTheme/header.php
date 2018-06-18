49aba5cbb31e4387fc3412ec0dec6b1b94e73c61
Paul Wr <paul.w35@live.com>
2018-06-18 14:16:47 +0000

    header image tag added

--- wp-content/themes/SkeletonTheme/header.php
+++ wp-content/themes/SkeletonTheme/header.php
@@ -7,5 +7,6 @@
 </head>
 <body <?php body_class();?> > <!-- add wp body classes. The body_class() function can take string args for classes -->
     <div id="wrapper">
+        <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
         <h1>This is the header</h1>
         <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'your-nav-class')) ?> <!-- call menu and specify where to find -->
